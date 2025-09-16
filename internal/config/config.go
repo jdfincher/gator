276f39a017889b74c0ed4f18ed9467ab430f8de2
jdfincher <jessefincher@protonmail.com>
2025-09-16 10:47:47 -0500

    commands

---
+++ internal/config/config.go
@@ -0,0 +1,63 @@
+// Package config implements the json structs and methods for reading json structs
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	DBURL    string `json:"db_url"`
+	UserName string `json:"current_user_name"`
+}
+
+func Read() (*Config, error) {
+	config := new(Config)
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return config, err
+	}
+	data, err := os.ReadFile(configFilePath)
+	if err != nil {
+		return config, fmt.Errorf("error reading file -> %w \nwith path -> %v", err, configFilePath)
+	}
+	if err := json.Unmarshal(data, config); err != nil {
+		return config, fmt.Errorf("error decoding config -> %w", err)
+	}
+	return config, nil
+}
+
+func (c *Config) SetUser(name string) error {
+	c.UserName = name
+	if err := write(c); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getConfigFilePath() (string, error) {
+	const configFileName = ".gatorconfig.json"
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("error getting config filepath -> %w", err)
+	}
+	filepath := home + "/" + configFileName
+	return filepath, nil
+}
+
+func write(cfg *Config) error {
+	path, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		return fmt.Errorf("error encoding data for writing -> %w", err)
+	}
+	err = os.WriteFile(path, data, 0644)
+	if err != nil {
+		return fmt.Errorf("error writing file -> %w", err)
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,3 +1,79 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/jdfincher/gator/internal/config"
+)
+
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	commands map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	f := c.commands[cmd.name]
+	err := f(s, cmd)
+	if err != nil {
+		return fmt.Errorf("error: running %v command -> %w", cmd.name, err)
+	}
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) error {
+	c.commands[name] = f
+	return nil
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("error: login requires Username")
+	}
+	if err := s.cfg.SetUser(cmd.args[0]); err != nil {
+		return err
+	}
+	fmt.Printf("Username successfully set to %v\n", cmd.args[0])
+	return nil
+}
+
+func newCommands() *commands {
+	c := new(commands)
+	c.commands = make(map[string]func(*state, command) error)
+	return c
+}
+
+func main() {
+	var err error
+	state := new(state)
+	state.cfg, err = config.Read()
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	coms := newCommands()
+	if err = coms.register("login", handlerLogin); err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Printf("error: missing command arguments, use gator 'command_name'\n")
+		os.Exit(1)
+	}
+	usercom := command{
+		name: strings.ToLower(args[1]),
+		args: args[2:],
+	}
+	if err := coms.run(state, usercom); err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+}
